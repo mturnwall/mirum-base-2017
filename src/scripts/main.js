aac33122dfb2eb555f1d3b5653b6eefd435323aa
Michael Turnwall <freelance@turnwall.net>
2018-03-12 15:50:03 -0700

    update eslint and config versions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 function doSomething() {
-    // console.log('Bang Bang Yo!');
+    console.log('Bang Bang Yo!');
 }
 
 window.addEventListener('DOMContentLoaded', function domLoad() {
     window.removeEventListener('DOMContentLoaded', domLoad);
     doSomething();
 });
-document.body.style.backgroundColor = '#0f0';
+document.body.style.backgroundColor = '#0ff';
 
 // tell webpack this file is available for HMR
 if (module.hot) {
